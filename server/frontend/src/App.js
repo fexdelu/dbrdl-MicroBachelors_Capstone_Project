fe0630b2386734c8de93b520fd736ace72d92149
Federico Lumma <federico.lumma@gmail.com>
2022-07-18 14:53:43 -0300

    Added chatbot to homepage

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,9 +3,17 @@ import React from 'react';
 import HomePage from './homepage.js';
 import ContactUs from './contactus.js';
 
-/*
-Copy the Web Chat code from Watson Assitant and paste it here
-*/
+window.watsonAssistantChatOptions = {
+    integrationID: "e16a9480-2ff6-43f6-aad9-bfd3e49827e5", // The ID of this integration.
+    region: "us-south", // The region your integration is hosted in.
+    serviceInstanceID: "4d9de83e-7847-4d76-b21c-7892ab52f631", // The ID of your service instance.
+    onLoad: function(instance) { instance.render(); }
+  };
+setTimeout(function(){
+  const t=document.createElement('script');
+  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js"
+  document.head.appendChild(t);
+});
 
 class App extends React.Component {
 
